7a1e5ef1daec421c0308148adaf652eb54488efa
zargoe <1042075661@qq.com>
2023-02-27 14:40:36 +0800

    first

---
+++ src/main/java/com/zargoe/git/test.java
@@ -0,0 +1,11 @@
+package com.zargoe.git;
+
+/**
+ * @author liu_zhiguo
+ * @create 2023-02-27 14:38
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("666");
+    }
+}
